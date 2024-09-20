4891e4997721f88f8a2309523deaada007d7c33e
Aleksandr Slinkov <sanch971@mail.ru>
2024-09-20 21:51:49 +0300

    Revert "4"
    This reverts commit 2224f3112c67b75fca392d523dc2db2081aa6a84.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}!')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('I need something to drink.!!!!$$$$$HAHAHAHAHAHAH')
+    print('I need something to drink.!!!!$$$$$')
 
 
 # Press the green button in the gutter to run the script.
